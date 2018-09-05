8ee594ca43d6c2ba365a0b9b44af4b206ee988a3
Agustin Izaguirre <Agustinizag1@gmail.com>
2018-09-05 18:06:20 -0300

    HSQL and Unit Testing on Persistance added

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.models.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:schema.sql")
+public class UserJdbcDaoTest {
+    private static final String PASSWORD = "Password";
+    private static final String USERNAME = "Username";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private UserJdbcDao userDao;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+    }
+
+    @Test
+    public void testCreate() {
+        final User user = userDao.create(USERNAME);
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals(USERNAME, user.getName());
+        Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "users"));
+    }
+}
---
+++ persistence/src/test/resources/schema.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE IF NOT EXISTS users(
+    userid INTEGER IDENTITY PRIMARY KEY,
+    username varchar(100)
+);
+
