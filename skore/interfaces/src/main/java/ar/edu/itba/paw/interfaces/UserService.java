9325aa38fe86a9846dfbe98df9a789663e7a039e
DonosoNaumczuk <adonoso@itba.edu.ar>
2018-10-03 03:53:47 -0300

    Cleaning repository

--- skore/interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.interfaces;
-
-import ar.edu.itba.paw.models.User;
-
-public interface UserService {
-    public User findById(String id);
-
-    public User create(String username);
-}
