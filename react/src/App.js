bf5cf6de859a48f9337cdc72f7cfa0acb780e1d2
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-01-12 00:34:03 -0300

    validation on component did mount to avoid initializing i18next two times

--- react/src/App.js
+++ react/src/App.js
@@ -4,10 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import i18next from 'i18next';
 import Accounts from './components/Accounts';
 import NavBar from './components/NavBar';
-import getProfileByUsername from './services/UserService';
+// import getProfileByUsername from './services/UserService';
 import CreateUserForm from './components/forms/CreateUserForm';
 import store from "./redux/store";
-import showResults from "./ShowResults";
 import './css/main.css';
 import UserProfile from './components/userProfile/UserProfile';
 
@@ -17,17 +16,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      account: {}
+      account: {},
+      translation: false
     }
   }
 
-  async componentDidMount() {   
-    let account = await getProfileByUsername('donosonaumczuk');//TODO remove
-    this.setState({ account: account });
+  initializeI18next = async () => {
+    if(!this.setState.translation) {
+      await i18next.init();
+      this.setState({
+        translation: true
+      });
+    }
+  }
+
+  async componentDidMount() {
+    this.initializeI18next();   
+    // let account = await getProfileByUsername('donosonaumczuk');//TODO remove
+    // this.setState({ account: account });
   }
 
   render() {
-
+   
+    if(!this.state.translation) {
+      //TODO return a spinner os something
+      return (<React.Fragment></React.Fragment>);
+    }
     return (
       <Provider store={store}>
       <div>
