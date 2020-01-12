65081949c0379e68e2006f6e1dc26d698921924c
AgustinIzaguirre <Agustinizag1@gmail.com>
2020-01-12 14:53:10 -0300

    image added to profile (#15)
    * image added to profile
    
    * profile image moved outside profile container
    
    * now username is accesed via path param instead of hardcoded
    
    * todo commented

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import i18next from 'i18next';
 import Accounts from './components/Accounts';
 import NavBar from './components/NavBar';
-import getProfileByUsername from './services/UserService';
+import UserService from './services/UserService';
 import CreateUserForm from './components/forms/CreateUserForm';
 import store from "./redux/store";
 import showResults from "./ShowResults";
@@ -22,8 +22,8 @@ class App extends Component {
   }
 
   async componentDidMount() {   
-    let account = await getProfileByUsername('donosonaumczuk');//TODO remove
-    this.setState({ account: account });
+    // let account = await UserService.getProfileByUsername('donosonaumczuk');//TODO add when /users enadpoint created
+    // this.setState({ account: account }); TODO add when /users endpoint created
   }
 
   render() {
@@ -40,9 +40,7 @@ class App extends Component {
             <Route path="/createUser">
               <CreateUserForm onSubmit={showResults}/>
             </Route>
-            <Route path="/user">
-              <UserProfile username="donosonaumczuk"/>{/* TODO obtain user from url or click event */} 
-            </Route>
+            <Route path="/user/:username" component={UserProfile} />
           </Switch>
         </Router>
       </div>
--- react/src/components/userProfile/UserProfile.js
+++ react/src/components/userProfile/UserProfile.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Proptypes from 'prop-types';
-import getProfileByUsername from '../../services/UserService';
+import UserService from '../../services/UserService';
 import UserData from './UserData';
+import UserImage from './UserImage';
 
 class UserProfile extends Component {
     constructor(props) {
         super(props);
-        const { username } = this.props;
+        const { username } = this.props.match.params;
+
         this.state = {
             username: username,
-            currentUser: {}
+            currentUser: {},
+            imageUrl: null
         }
     }
 
     async componentDidMount() {   
-        let currentUser = await getProfileByUsername(this.state.username)
-        this.setState({ currentUser: currentUser });
+        let currentUser = await UserService.getProfileByUsername(this.state.username)
+        const imageUrl = this.getImageUrl(currentUser.links);
+        
+        this.setState({ currentUser: currentUser, 
+                        imageUrl: imageUrl 
+                    });
     }
 
     locationData = (location) => {
@@ -46,12 +53,25 @@ class UserProfile extends Component {
         );
     }
     
+    getImageUrl = links => {
+        let imageUrl;
+        links.map(link => {
+            if(link.rel === "image") {
+                imageUrl = link.href;
+            }
+        } )
+        return imageUrl;
+    }
+
     render() {
         const currentUser = this.state.currentUser;
+        const imageUrl = this.state.imageUrl;
+        //TODO check when winrate is negative if it is a valid value
         return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="container-fluid sign-in-container offset-sm-2 col-sm-8 offset-md-3 col-md-6 offset-lg-3 col-lg-6 offset-xl-4 col-xl-4">
+                        <UserImage styleClass="profile-pic" imageUrl={imageUrl} />    
                         <div className="container-fluid profile-container bg-white rounded-border">
                             <UserData styleClass="profile-name" value={currentUser.firstName + " " + currentUser.lastName} />
                             <UserData styleClass="profile-username" value={currentUser.username} />
@@ -67,7 +87,7 @@ class UserProfile extends Component {
 }
 
 UserProfile.propTypes = {
-    username: Proptypes.string.isRequired
+    match: Proptypes.object.isRequired
 }
 
 export default UserProfile;
