3b7516db3ff12002ea39bec5e4dbdcac332c0e82
AgustinIzaguirre <Agustinizag1@gmail.com>
2020-01-15 15:37:32 -0300

    Add matches to user profile (#20)
    * image added to profile
    
    * Profile image moved outside profile container
    
    * image validation when click on cancel
    
    * Sport info added to game on profile and sport display name and sport image link were added to gameDto
    
    * Added results to match
    
    * User profile use spinner until it has data
    
    * Location, competitivity and location added to user profile and location added to gameDto
    
    * Profile image on user match redirect to creator profile
    
    * Profile edit buttons added
    
    * NavBar and profile anchore tags updated to Link
    
    * Redirect to error page if path does not match

---
+++ react/src/components/match/MatchLocation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+const MatchLocation = ({ address }) => {
+  
+    return (
+         <div className="row">
+            <div className="col">
+                <p>
+                    <span className="location-icon mr-2 fas fa-map-marker-alt"></span>
+                    {address}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+MatchLocation.propTypes = {
+    address: Proptypes.string.isRequired
+}
+
+export default MatchLocation;
