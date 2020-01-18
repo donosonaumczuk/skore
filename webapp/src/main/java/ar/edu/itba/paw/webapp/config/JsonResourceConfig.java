5afa9931a2108f8e9e462ed02bf6dcae61b7a707
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-01-18 19:04:59 -0300

    Mapper config updated because now we do it manually

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/JsonResourceConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/JsonResourceConfig.java
@@ -12,9 +12,7 @@ public class JsonResourceConfig extends ResourceConfig {
     public JsonResourceConfig() {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.ACCEPT_FLOAT_AS_INT);
-        objectMapper.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);//TODO:CHECK validate
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNRESOLVED_OBJECT_IDS);//TODO:CHECK validate
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); /* Check done by us manually */
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT); //TODO: To look better in browser, remove for prod
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         register(new JacksonJaxbJsonProvider(objectMapper, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS));
