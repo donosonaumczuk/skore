541c1befd42c66fc2353d37853061280a9e154f1
AgustinAlejoDammiano <adammiano@itba.edu.ar>
2020-01-18 16:29:20 -0300

    Feature/user endpoint (#21)
    * add delete user endpoint
    
    * change user dao an service
    
    * add get and post
    
    * add post user
    
    * add verify endpoint
    
    * add security
    
    * minor fix
    
    * minors fixes
    
    * can update mail
    
    * minor fix
    
    * minors fixes
    
    * minor fix

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/PlaceDto.java
@@ -0,0 +1,58 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.models.Place;
+
+public class PlaceDto {
+
+    private String country;
+    private String state;
+    private String city;
+    private String street;
+
+    public PlaceDto() {
+
+    }
+
+    private PlaceDto(Place place) {
+        this.country    = place.getCountry();
+        this.state      = place.getState();
+        this.city       = place.getCity();
+        this.street     = place.getStreet();
+    }
+
+    public static PlaceDto from(Place place) {
+        return new PlaceDto(place);
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    public void setState(final String state) {
+        this.state = state;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public void setStreet(final String street) {
+        this.street = street;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
