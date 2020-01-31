829eaf93e8353a171edc8d10c67f5cc9395b61a2
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-01-30 22:03:38 -0300

    Birthday and email regex updated

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validators/UserValidators.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validators/UserValidators.java
@@ -75,7 +75,7 @@ public class UserValidators {
 
     private static ImmutableMap.Builder<String, Validator<JSONObject>> baseFieldValidatorMapOf(final String log) {
         return new ImmutableMap.Builder<String, Validator<JSONObject>>()
-                .put(EMAIL, ValidatorFactory.fieldIsStringAndMatchesRegexOf(EMAIL, Pattern.compile(".*@.*"),
+                .put(EMAIL, ValidatorFactory.fieldIsStringAndMatchesRegexOf(EMAIL, Pattern.compile("^[a-zA-ZñÑ0-9_.-]+@[a-zA-Z0-9]+(\\.[A-Za-z]+)+"),
                         "an email", log)) //TODO: find an standard email pattern
                 .put(FIRST_NAME, ValidatorFactory.fieldIsStringValidatorOf(FIRST_NAME, log))
                 .put(LAST_NAME,  ValidatorFactory.fieldIsStringValidatorOf(LAST_NAME, log))
@@ -83,7 +83,7 @@ public class UserValidators {
                         Pattern.compile("([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])|( *)"), //TODO: add a better regex
                         "a cellphone number", log)) //TODO: maybe exists an standard pattern for cellphone
                 .put(BIRTHDAY, ValidatorFactory.fieldIsStringAndMatchesRegexOf(BIRTHDAY,
-                        Pattern.compile("[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]"), "a date", log)) //TODO: maybe exists an standard pattern for this date format
+                        Pattern.compile("[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]"), "a date", log)) //TODO: maybe exists an standard pattern for this date format
                 .put(PASSWORD, ValidatorFactory.fieldIsStringValidatorOf(PASSWORD, log))
                 .put(IMAGE, ValidatorFactory.fieldIsStringValidatorOf(IMAGE, log)); //TODO: call the ImageValidator ?
     }
