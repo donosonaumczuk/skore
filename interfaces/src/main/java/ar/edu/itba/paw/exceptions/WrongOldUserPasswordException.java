1dfc1aaa15c744db20971d4c7440ec61739ae766
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-02-03 18:11:05 -0300

    WrongOldUserPasswordException added

---
+++ interfaces/src/main/java/ar/edu/itba/paw/exceptions/WrongOldUserPasswordException.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.paw.exceptions;
+
+public class WrongOldUserPasswordException extends RuntimeException {
+
+    private WrongOldUserPasswordException(final String username) {
+        super("Invalid old password for user '" + username + "'");
+    }
+
+    public static WrongOldUserPasswordException ofUsername(final String username) {
+        return new WrongOldUserPasswordException(username);
+    }
+}
