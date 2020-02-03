9698c8efc8426cbb7856ea2aa34c84f19c05d1d5
agustin <adammiano@itba.edu.ar>
2020-02-03 15:05:28 -0300

    minor fix

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -112,7 +112,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     public RequestMatcher optionalAuthEndpointsMatcher() {
         return new OrRequestMatcher( //TODO make list
                 new RegexRequestMatcher("/api/matches/temporal/?", "POST"),//TODO: maybe not needed
-                new RegexRequestMatcher("/api/matches/\\w+/players/code/\\w+/?", "POST")
+                new RegexRequestMatcher("/api/matches/\\w+/players/code/\\w+/?", "DELETE")
         );
     }
 
