e2c75ba31891d172b98cb731a8ae5e4b28a364a5
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-02-08 14:37:05 -0300

    sugegsted changes applied

--- webapp/src/main/java/ar/edu/itba/paw/webapp/constants/HeaderConstants.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/constants/HeaderConstants.java
@@ -3,7 +3,6 @@ package ar.edu.itba.paw.webapp.constants;
 public class HeaderConstants {
 
     public static final String TOKEN_HEADER = "X-TOKEN";
-
     public static final String CODE_HEADER = "X-CODE";
 
 }
