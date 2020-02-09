af028b30559e8efc8d821759d1ce8afce334089d
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-02-09 13:33:19 -0300

    Remove private constructor from URLConstants

--- webapp/src/main/java/ar/edu/itba/paw/webapp/constants/URLConstants.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/constants/URLConstants.java
@@ -9,10 +9,6 @@ import javax.ws.rs.core.UriBuilder;
 @Component
 public final class URLConstants {
 
-    private URLConstants() {
-        /* Utility class */
-    }
-
     public static String BASE_URL;
     public static final String API_PATH = "api/";
     public static String API_BASE_URL;
@@ -20,19 +16,19 @@ public final class URLConstants {
     private static UriBuilder BASE_URL_BUILDER;
     private static UriBuilder API_BASE_URL_BUILDER;
 
-    public static UriBuilder getApiBaseUrlBuilder() {
-        return API_BASE_URL_BUILDER.clone();
-    }
-
-    public static UriBuilder getBaseUrlBuilder() {
-        return BASE_URL_BUILDER.clone();
-    }
-
     @Autowired
     public URLConstants(Environment environment) {
         URLConstants.BASE_URL = environment.getProperty("url.backend.base");
         URLConstants.BASE_URL_BUILDER = UriBuilder.fromUri(BASE_URL);
         URLConstants.API_BASE_URL = getBaseUrlBuilder().path(API_PATH).toTemplate();
         URLConstants.API_BASE_URL_BUILDER = UriBuilder.fromUri(API_BASE_URL);
+    }
+
+    public static UriBuilder getApiBaseUrlBuilder() {
+        return API_BASE_URL_BUILDER.clone();
+    }
+
+    public static UriBuilder getBaseUrlBuilder() {
+        return BASE_URL_BUILDER.clone();
     }
 }
