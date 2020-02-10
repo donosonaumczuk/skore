cee98b3a59cd90882bd1a5be2f431913631ec0fd
AgustinAlejoDammiano <adammiano@itba.edu.ar>
2020-02-10 14:20:07 -0300

    Update interfaces/src/main/java/ar/edu/itba/paw/exceptions/invalidstate/EntityInvalidStateException.java
    Co-Authored-By: Alan <adonoso@itba.edu.ar>

--- interfaces/src/main/java/ar/edu/itba/paw/exceptions/invalidstate/EntityInvalidStateException.java
+++ interfaces/src/main/java/ar/edu/itba/paw/exceptions/invalidstate/EntityInvalidStateException.java
@@ -3,6 +3,6 @@ package ar.edu.itba.paw.exceptions.invalidstate;
 public abstract class EntityInvalidStateException extends RuntimeException {
 
     public EntityInvalidStateException(final String entityName, final String entityId, final String reason) {
-        super(entityName + " '" + entityId + "', " + reason);
+        super(entityName + " '" + entityId + "' " + reason);
     }
 }
