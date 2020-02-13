5dbe7a86a160775facbb2bc8e671205e22f32884
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-02-13 12:35:33 -0300

    General exception mapper improved

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/ExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/ExceptionMapper.java
@@ -1,10 +1,11 @@
 package ar.edu.itba.paw.webapp.exceptions;
 
-import ar.edu.itba.paw.webapp.constants.MessageConstants;
 import ar.edu.itba.paw.webapp.dto.ApiErrorDto;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
@@ -13,14 +14,25 @@ public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exceptio
 
     @Override
     public Response toResponse(Exception e) {
-        LoggerFactory.getLogger(ExceptionMapper.class).error("general exception mapper", e);
+        LoggerFactory.getLogger(ExceptionMapper.class).error("A generic exception was been thrown", e);
+        Response.StatusType type = getStatusType(e);
         return Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(ApiErrorDto.of(HttpStatus.INTERNAL_SERVER_ERROR, MessageConstants.SERVER_ERROR_GENERIC_MESSAGE))
+                .status(type.getStatusCode())
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(ApiErrorDto.of(HttpStatus.valueOf(type.getStatusCode())))
                 .build();
+    }
+
+    private Response.StatusType getStatusType(Exception e) {
+        if (e instanceof WebApplicationException) {
+            return ((WebApplicationException) e).getResponse().getStatusInfo();
+        } else {
+            return Response.Status.INTERNAL_SERVER_ERROR;
+        }
     }
 }
 
 //public class ExceptionMapper {
 //    /* TODO: This is to see the full exception in the response... */
+//    /* TODO: Remove me when webapp is finished! */
 //}
