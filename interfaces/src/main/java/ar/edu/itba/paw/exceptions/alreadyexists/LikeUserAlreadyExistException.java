ca59253ca7adff5b1d95bffbecbf86e304c36cd0
agustin <adammiano@itba.edu.ar>
2020-02-14 22:20:03 -0300

    make like exception more user friendly

--- interfaces/src/main/java/ar/edu/itba/paw/exceptions/alreadyexists/LikeUserAlreadyExistException.java
+++ interfaces/src/main/java/ar/edu/itba/paw/exceptions/alreadyexists/LikeUserAlreadyExistException.java
@@ -5,7 +5,7 @@ public class LikeUserAlreadyExistException extends EntityAlreadyExistsException 
     private final static String ENTITY_NAME = "Like user";
 
     private LikeUserAlreadyExistException(final String username, final String usernameOfLiked) {
-        super(ENTITY_NAME, username + "|" + usernameOfLiked);
+        super(ENTITY_NAME, usernameOfLiked + " from user " + username);
     }
 
     public static LikeUserAlreadyExistException ofUsernames(final String username, final String usernameOfLiked) {
