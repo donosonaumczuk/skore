4f1a8fabdca3d5556125556b8ed362b545363e05
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-02-16 00:00:58 -0300

    branch updated with add-sports-likes

---
+++ react/src/components/screens/Sports/components/SportLikeButton/index.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import ActionButton from '../../../../ActionButton';
+
+const getLikeButton = (isLogged, isLiked, likeSport, dislikeSport, sportName) => {
+if (!isLogged) {
+        return <Fragment></Fragment>;
+    }
+    if (isLiked) {
+        return (
+            <ActionButton buttonStyle="btn btn-negative join-button" 
+                        handleClick={dislikeSport} data={sportName}
+                        buttonText={null} fontAwesome="fas fa-thumbs-down mr-1" />
+        );
+    }
+    else {
+        return (
+            <ActionButton buttonStyle="btn btn-green join-button"
+                        handleClick={likeSport} data={sportName} 
+                        buttonText={null} fontAwesome="fas fa-thumbs-up mr-1" />
+        )
+    }
+}
+
+const SportLikeButton = ({ isLogged, isLiked, likeSport, dislikeSport, sportName }) => {
+    const likeButton = getLikeButton(isLogged, isLiked, likeSport, dislikeSport, sportName);
+    return (
+        <div className="row">
+            <div className="col offset-2">
+                {likeButton}
+            </div>
+        </div>        
+    );
+}
+
+SportLikeButton.propTypes = {
+    isLogged: PropTypes.string,
+    isLiked: PropTypes.bool,
+    likeSport: PropTypes.func.isRequired,
+    dislikeSport: PropTypes.func.isRequired,
+    sportName: PropTypes.string.isRequired
+}
+
+export default SportLikeButton;
