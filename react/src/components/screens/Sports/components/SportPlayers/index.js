fd442a19e9478d070428655a339502af32a6e578
Alan Donoso Naumczuk <donosonaumczuk@gmail.com>
2020-02-17 11:17:22 -0300

    Sport item improved

--- react/src/components/screens/Sports/components/SportPlayers/index.js
+++ react/src/components/screens/Sports/components/SportPlayers/index.js
@@ -4,13 +4,11 @@ import i18next from 'i18next';
 
 const SportPlayers = ({ playersQuantity }) => {
     return (
-        <div className="row">
-            <div className="col">
+            <div className="offset-1 col my-auto">
                 <i className="name-label fas fa-users mr-2"></i>
                 <label>{i18next.t('sports.playersPerTeam')}</label>
                  <span>{` ${playersQuantity}`}</span>
             </div>
-        </div>
     );
 }
 
